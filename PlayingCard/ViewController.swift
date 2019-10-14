3abe2683bd685c2b95c2c8f763a64ce547b2ab98
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2019-10-14 22:33:19 +0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Yurii Serediuk on 14.10.2019.
+//  Copyright © 2019 Yurii Serediuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
